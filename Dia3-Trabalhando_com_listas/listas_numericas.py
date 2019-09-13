7a1984ebf07ea301cea641f86ce35d498e2b69fc
luandadantas <ludanttas@gmail.com>
2019-09-12 23:43:23 -0300

    Listas Numéricas

---
+++ Dia3-Trabalhando_com_listas/listas_numericas.py
@@ -0,0 +1,20 @@
+numbers = list(range(1, 6))
+print(numbers)
+
+numbers2 = list(range(2, 11, 2))
+print(numbers2)
+print("\n")
+
+#squares
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+print(squares) 
+print("\n")   
+
+#Contando até 20
+numeros = list(range(1,21))
+print(numeros)
+print("\n")
+
