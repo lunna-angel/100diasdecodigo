4dda43a45994369d814867cdabf6a6907c220c92
luandadantas <ludanttas@gmail.com>
2019-09-13 00:35:45 -0300

    Praticando com mais algumas listas numéricas

--- Dia3-Trabalhando_com_listas/listas_numericas.py
+++ Dia3-Trabalhando_com_listas/listas_numericas.py
@@ -18,3 +18,41 @@ numeros = list(range(1,21))
 print(numeros)
 print("\n")
 
+#Um milhão
+milhao = []
+for i in range(0,1000000):
+    milhao.append(i + 1)
+print(milhao)    
+
+#Somando um milhão
+print(min(milhao))
+print(max(milhao))
+print(sum(milhao))
+print("\n")
+
+#Números Ímpares
+impares = []
+for impar in range(1, 20, 2):
+    impares.append(impar)
+print(impares)    
+print("\n")
+
+#Três
+multiplos = []
+for multiplo in range(3, 30, 3):
+    multiplos.append(multiplo)
+print(multiplos)    
+print("\n")
+
+#Cubos
+cubos = []
+for cubo in range(1, 11):
+    i = cubo ** 3
+    cubos.append(i)
+print(cubos)    
+print("\n")
+
+#Comprehension de cubos
+cubos = [cubo ** 3 for cubo in range(1,11)]
+print(cubos)
+
