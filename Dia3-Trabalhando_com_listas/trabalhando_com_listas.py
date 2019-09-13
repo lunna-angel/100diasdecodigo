7181ddf2cdce8c74b257feb3df224ef4566eaa5b
luandadantas <ludanttas@gmail.com>
2019-09-12 22:07:19 -0300

    Trabalhando com Listas.

---
+++ Dia3-Trabalhando_com_listas/trabalhando_com_listas.py
@@ -0,0 +1,18 @@
+magicians = ["alice", "david", "carolina"]
+for magician in magicians:
+    print(magician.title() + ", that was a great trick")
+    print("I can't wait to see yout next trick, " + magician.title() + ".\n")
+
+print("Thank you, everyone. That was a great magic show!\n\n")    
+
+# Pizzas
+sabores_pizzas = ["Marguerita", "Quatro queijos", "Brócolis"]
+for pizza in sabores_pizzas:
+    print("Gosto de Pizza de " + pizza + ".")
+print("\n")
+
+# Animais
+animais = ["papagaio", "cachorro", "gato"]
+for animal in animais:
+    print("O " + animal + " é um ótimo animal de estimação")
+print("Qualquer um desses animais seria um ótimo animal de estimação.\n")    
