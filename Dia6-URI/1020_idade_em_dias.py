70f10edc88b1271521d53a0335f93ea3fa93d3ef
luandadantas <ludanttas@gmail.com>
2019-09-15 22:45:52 -0300

    Resolução do desafio 1020 do URI

---
+++ Dia6-URI/1020_idade_em_dias.py
@@ -0,0 +1,26 @@
+
+''' 
+Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias.
+Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. 
+Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. 
+Este é apenas um exercício com objetivo de testar raciocínio matemático simples.
+
+Entrada: O arquivo de entrada contém um valor inteiro.
+Saída: Imprima a saída conforme exemplo fornecido.
+
+Exemplo de Entrada: 400
+Exemplo de Saída: 1 ano(s)
+                  1 mes(es)
+                  5 dia(s)
+'''  
+
+idade = int(input())
+ano = idade//365
+print(str(ano) + " ano(s)")
+
+resto_ano = idade%365
+mes = resto_ano//30
+print(str(mes) + " mes(es)")
+
+dia = resto_ano%30
+print(str(dia) + " dia(s)")
