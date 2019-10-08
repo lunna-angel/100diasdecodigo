164d8d7609a2c4496689a9d5101ac07516b7fa79
luandadantas <ludanttas@gmail.com>
2019-10-08 00:10:46 -0300

    Exercício do livro Curso Intensivo de Python

---
+++ Dia25-Sintaxe_if-elif-else/alien_color.py
@@ -0,0 +1,8 @@
+alien_color = "vermelho"
+
+if alien_color == verde:
+    print("O jogador acabou de ganhar 5 pontos")
+elif alien_color == amarelo
+    print("O jogador acabou de ganhar 10 pontos") 
+else:
+    print("O jogador acabou de ganhar 15 pontos")
