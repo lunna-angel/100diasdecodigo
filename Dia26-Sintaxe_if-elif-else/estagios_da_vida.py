da28af7e8e117c1dd9dc55ee0d3e94e1817fd26f
luandadantas <ludanttas@gmail.com>
2019-10-08 21:39:45 -0300

    Exercício do livro Curso Intensivo de Python

---
+++ Dia26-Sintaxe_if-elif-else/estagios_da_vida.py
@@ -0,0 +1,16 @@
+age = 18
+
+if age < 2:
+    print("É um bebê")
+elif 2 <= age < 4:
+    print("É uma criança")
+elif 4 <= age < 13:
+    print("É um(a) gatoto(a)")
+elif 13 <= age < 20:
+    print("É um(a) adolescente")
+elif 20 <= age < 65:
+    print("É um adulto")
+else:
+    print("É idosa")
+
+
---
+++ Dia26-Sintaxe_if-elif-else/fruta_favorita.py
@@ -0,0 +1,10 @@
+frutas_favoritas = ['laranja', 'maça', 'uva']
+
+if 'pera' in frutas_favoritas:
+    print("Você realmente gosta de pera")
+if 'laranja' in frutas_favoritas:
+    print("Você realemnte gosta de laranja")
+if 'banana' in frutas_favoritas:
+    print("Você realmente gosta de banana")
+if 'uva' in frutas_favoritas:
+    print("Você realmente gosta de uva")         
