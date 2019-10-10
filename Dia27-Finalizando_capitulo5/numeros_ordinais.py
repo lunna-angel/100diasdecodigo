4b94afc0794adfe48a108850fd89512ebb9890f5
luandadantas <ludanttas@gmail.com>
2019-10-09 23:46:55 -0300

    Exercício Faça você mesmo - Capítulo 5

---
+++ Dia27-Finalizando_capitulo5/numeros_ordinais.py
@@ -0,0 +1,11 @@
+numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+for numero in numeros:
+    if numero == '1':
+        print("1st")
+    elif numero == '2':
+        print("2nd")
+    elif numero == '3':
+        print("3rd")
+    else:
+        print(numero + "th")            
