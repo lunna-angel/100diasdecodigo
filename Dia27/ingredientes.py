aa48889161cbc0d85131fa66e7b88ca1d36d5e37
luandadantas <ludanttas@gmail.com>
2019-10-09 22:04:18 -0300

    Exercício do livro Curso Intensivo de Python - capítulo 5

---
+++ Dia27/ingredientes.py
@@ -0,0 +1,11 @@
+
+ingredientes = ['tomate', 'cogumelo', 'queijo']
+
+for ingrediente in ingredientes:
+    if ingrediente == 'cogumelo':
+        print("Desculpa, nós não temos esse ingrediente.")
+    else:    
+        print("adicionando " + ingrediente + ".")
+
+print("\n A pizza está finalizada!")
+
