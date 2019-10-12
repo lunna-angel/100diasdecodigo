300ba1987953a2103dda6dcdba3da38dfce287a1
luandadantas <ludanttas@gmail.com>
2019-10-12 20:15:38 -0300

    Exercício Faça você mesmo - Capítulo 6

---
+++ Dia30-brincando_com_dicionarios/usuario.py
@@ -0,0 +1,9 @@
+usuario_0 = {
+    'username': 'Fátima',
+    'primeiro': 'Maria',
+    'ultimo': 'Santos',
+}
+
+for chave, valor in usuario_0.items():
+    print("\nChave: " + chave)
+    print("Valor: " + valor)
