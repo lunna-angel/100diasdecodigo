675718dd9db4fe60bf54e5c2c30687fdc25e5fd0
luandadantas <ludanttas@gmail.com>
2019-10-12 00:18:36 -0300

    Exercício - Linguagem Favorita

---
+++ Dia29-brincando_com_dicionarios/linguagem_favorita.py
@@ -0,0 +1,8 @@
+linguagem_favorita = {
+    'jen': 'python',
+    'sarah': 'C',
+    'eduardo': 'ruby',
+    'phil': 'python',
+}
+
+print("A liguagem favorita de Sarah é " + linguagem_favorita['sarah'].title() + ".")
