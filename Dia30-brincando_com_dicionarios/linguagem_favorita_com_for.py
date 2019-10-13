6b5aae27d52ba5a48248b43f3f94d48e11e7a489
luandadantas <ludanttas@gmail.com>
2019-10-12 21:21:38 -0300

    Exercício Faça você mesmo - Capítulo 6

---
+++ Dia30-brincando_com_dicionarios/linguagem_favorita_com_for.py
@@ -0,0 +1,9 @@
+linguagem_favorita = {
+    'jen': 'python',
+    'sara': 'C',
+    'gustavo': 'ruby',
+    'leo': 'python',
+}
+
+for nome, linguagem in linguagem_favorita.items():
+    print("A linguagem favorita de " + nome.title() + " é " + linguagem.title())
