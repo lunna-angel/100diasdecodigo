22bdd58b2515c6d997b74b4d5234399675a1e585
luandadantas <ludanttas@gmail.com>
2019-10-13 23:41:47 -0300

    Exercício Faça você mesmo - Capítulo 6

---
+++ Dia31-brincando_com_dicionarios/animais_estimacao_ordenado.py
@@ -0,0 +1,10 @@
+animais_estimacao = {
+    'paloma': 'porquinho-da-india',
+    'matheus': 'cachorro',
+    'maria': 'coelho',
+    'luanda': 'gato',
+    'idylla': 'rato',
+}
+
+for nome in sorted(animais_estimacao.keys()):
+    print(nome.title() + ", seu bichinho é muito fofo.")
