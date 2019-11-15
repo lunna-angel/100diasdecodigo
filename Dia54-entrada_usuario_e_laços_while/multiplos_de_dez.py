8591e55be030789e9342142878f9b11f7d3a1cda
luandadantas <ludanttas@gmail.com>
2019-11-15 01:47:29 -0300

    faça você mesmo - múltiplos de dez

---
+++ Dia54-entrada_usuario_e_laços_while/multiplos_de_dez.py
@@ -0,0 +1,7 @@
+numero = input("Entre com um número: ")
+numero = int(numero)
+
+if numero % 10 == 0:
+    print("Esse número é múltiplo de dez.")
+else:
+    print("Esse número não é múltiplo de dez.")    
