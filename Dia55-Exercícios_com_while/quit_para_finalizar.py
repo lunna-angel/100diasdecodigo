7961ffa510a94a9fe309f3b7f2b9017943d344ee
luandadantas <ludanttas@gmail.com>
2019-11-16 20:01:03 -0300

    Exercício com while

---
+++ Dia55-Exercícios_com_while/quit_para_finalizar.py
@@ -0,0 +1,7 @@
+x = "\n Me diga algo, que irei imprimir de volta para você."
+x += "\n Entre com 'quit' para finalizar o programa. "
+
+mensagem = ""
+while mensagem != 'quit':
+    mensagem = input(x)
+    print(mensagem)
