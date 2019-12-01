8ffa3f1ba84051ac65d2a3a3a3ee6926209f74e6
luandadantas <ludanttas@gmail.com>
2019-12-01 20:11:01 -0300

    Faça voce mesmo - funçoes - livro favorito

---
+++ Dia70-funcoes/livro_favorito.py
@@ -0,0 +1,6 @@
+
+def livro_favorito(titulo):
+    """exibe uma mensagem com o título do livro"""
+    print("Um dos meus livros favoritos é " + titulo.title() + ".")
+
+livro_favorito("Curso intensivo de Python.")
