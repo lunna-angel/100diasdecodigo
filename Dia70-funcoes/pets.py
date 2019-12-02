e4901b607330d2763f6872844bf419b77c65cdc6
luandadantas <ludanttas@gmail.com>
2019-12-01 21:00:34 -0300

    Faça voce mesmo - funçoes - pets

---
+++ Dia70-funcoes/pets.py
@@ -0,0 +1,7 @@
+
+def descricao_animal(animal, nome):
+    "Exibe informações de um animal de estimação - Usando argumentos posicionais"
+    print("Eu tenho um " + animal.title() + ".")
+    print("O nome dele é " + nome.title() + ".")
+
+descricao_animal("cachorro", "nico")    
