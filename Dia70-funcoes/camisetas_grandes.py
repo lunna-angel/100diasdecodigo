f5435da7da83771e56281d0c4812588120c4024d
luandadantas <ludanttas@gmail.com>
2019-12-01 23:43:50 -0300

    Faça voce mesmo - funçoes - camisetas_grandes

---
+++ Dia70-funcoes/camisetas_grandes.py
@@ -0,0 +1,10 @@
+
+def camiseta(tamanho='G', frase='Eu amo python'):
+    print("O tamanho da camiseta será " + tamanho + ".")
+    print("A frase escrita na camiseta será " + frase + ".\n")
+
+camiseta("P", "Just Python")
+camiseta(tamanho='M')
+camiseta(frase='Python é ótimo')    
+
+
