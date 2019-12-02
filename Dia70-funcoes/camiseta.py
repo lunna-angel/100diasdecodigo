9738b05caaeac4791f02a6dde62294596808e517
luandadantas <ludanttas@gmail.com>
2019-12-01 23:22:35 -0300

    Faça voce mesmo - funçoes - camiseta

---
+++ Dia70-funcoes/camiseta.py
@@ -0,0 +1,8 @@
+
+def camiseta(tamanho, frase):
+    print("O tamanho sa camisa será " + tamanho.title() + ".")
+    print("A mensagem estampada é " + frase.title() + ".")
+    print("\n")
+
+camiseta("p", "Code like a girl")
+camiseta(tamanho = "g", frase = "We love python")   
